8843f2c5b553c3d119d3e4535b3af0966f843b76
Exit95 <danapfelmichael7@gmail.com>
2025-12-01 02:55:19 +0100

    update2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
+  security: {
+    checkOrigin: false
+  },
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
